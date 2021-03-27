57ab31dc819e5c8104f948d3b299c910d2bbddb7
Akide <mail@llycloud.com>
2021-03-27 20:58:13 +1030

    prac 4 to 4.6

--- prac4/routes/index.js
+++ prac4/routes/index.js
@@ -227,6 +227,69 @@ router.get('/', function(req, res, next) {
 })();
 
 
+// task 4-5
+
+(() => {
+
+  // flag for check did /accept has been access before or not.
+  let flag = false;
+
+  router.get('/accept', function (req, res, next) {
+
+    flag = true;
+    res.status(200);
+    res.send();
+
+  });
+
+  router.get('/content.ajax', function (req, res, next) {
+
+    if (flag === false) {
+      res.status(403);
+      res.send();
+    } else {
+      res.send(`
+        <p>paragraph 1</p>
+        <p>paragraph 2</p>
+      `);
+    }
+
+  });
+
+
+})();
+
+// task 4-6
+
+(() => {
+
+  const images = [
+    { uri:'photo-1539154444419-e31272d30a31.jpg', description:'medium-coated black-and-white dog near grass during daytime' },
+    { uri:'photo-1553882809-a4f57e59501d.jpg',    description:'black and tan Belgian dog' },
+    { uri:'photo-1554196721-b507d7e86ee9.jpg',    description:'gray dog standing on grass' },
+    { uri:'photo-1555661059-7e755c1c3c1d.jpg',    description:'black dog behind plants' },
+    { uri:'photo-1555991415-1b04a71f18c5.jpg',    description:'adult white Samoyed beside man and woman' },
+    { uri:'photo-1558121591-b684092bb548.jpg',    description:'white and black dog lying on sofa' },
+    { uri:'photo-1559440165-065646588e9a.jpg',    description:'person holding dog leash short-coat black and white dog' },
+    { uri:'photo-1560160643-7c9c6cb07a8b.jpg',    description:'short-coated brown and white dog' },
+    { uri:'photo-1562220058-1a0a019ab606.jpg',    description:'pet dog laying on sofa' },
+    { uri:'photo-1565194481104-39d1ee1b8bcc.jpg', description:'short-coated gray dog' }
+  ];
+
+  let index = -1;
+  router.get('/images.json', function (req, res, next) {
+
+    index++;
+
+    if (index === images.length) {
+      index = 0;
+    }
+
+    res.send(images[index]);
+  });
+
+})();
+
 
 
 module.exports = router;
