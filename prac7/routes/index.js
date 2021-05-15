abf123a0ab0fcd62d9e2bcea5877349fa91867fc
Akide <mail@llycloud.com>
2021-05-16 00:59:07 +0930

    Revert "prac 7 part 1"
    This reverts commit 4a9fc934b96c05dee2c6231ee30034afd0de08b6.

--- prac7/routes/index.js
+++ prac7/routes/index.js
@@ -6,8 +6,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
-// task 1.1
 (()=>{
   router.get('/brew',function (req, res, next) {
 
@@ -22,103 +20,6 @@ router.get('/', function(req, res, next) {
     }
 
   });
-})();
-
-
-// task 1.2
-(()=>{
-
-  let message = [];
-  let index = 0;
-
-  router.post('/pass-it-on', function (req, res, next) {
-
-
-    console.log(req.body.message);
-
-    if (req.body.message === "" || req.body.message === undefined || req.body.message === null) {
-      res.status(400).send();
-    } else {
-
-      message.push(req.body.message);
-
-      if (index === 0) {
-        res.send("first");
-      } else {
-        res.send(message[index-1]);
-      }
-
-      index++;
-
-    }
-
-
-  });
-})();
-
-// task 1.3
-
-(()=>{
-  router.post('/combine', function (req, res, next) {
-
-    // console.log(req.body.lines);
-
-    let resString = '';
-
-    for (const line of req.body.lines) {
-      resString += (line + req.body.suffix + '\n');
-    }
-
-    res.send(resString);
-  });
-})();
-
-
-//task 1.4
-
-(()=>{
-
-  let userArray = [];
-
-  router.post('/users/addpost', function (req, res, next) {
-    userArray.push(req.body);
-    res.send();
-  });
-
-  router.get('/users/getposts', function (req, res, next) {
-
-    const numbers = req.query.n;
-
-    let tempArray = [];
-
-    // console.log(numbers);
-
-    // task 1.5
-    if (req.query.n) {
-      for (let i = 0; i < parseInt(numbers) && i < userArray.length; i++) {
-        tempArray.push(userArray[i]);
-      }
-      res.send(tempArray);
-    } else {
-      res.send(userArray);
-    }
-
-    // res.send(userArray);
-
-
-
-  });
-
-  router.get('/users/getposts/id/:id', function (req, res, next) {
-
-    const id = req.params.id;
-    if (id && id < userArray.length) {
-      res.send(userArray[id]);
-    } else {
-      res.send(userArray);
-    }
-  });
-
 })();
 
 module.exports = router;
